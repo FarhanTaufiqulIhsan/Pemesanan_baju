043c21235a89f6b6d008f327775db7b2767c0733
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-04-05 09:18:41 +0700

    Add project files.

---
+++ Pemesanan_baju/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pemesanan_baju
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
