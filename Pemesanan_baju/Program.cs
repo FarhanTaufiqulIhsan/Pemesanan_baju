53ca759092fc8c69fab2015543f3d84f62a7dc87
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-04-05 09:25:15 +0700

    Menentukan namespace, membuat tampilan menu

--- Pemesanan_baju/Program.cs
+++ Pemesanan_baju/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace Pemesanan_baju
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Program pr = new Program();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Keoneksi Ke Database\n");
+                    Console.WriteLine("Masukkan User ID : ");
+                    string user = Console.ReadLine();
+                    Console.WriteLine("Masukkan database tujuan :");
+                    string db = Console.ReadLine();
+                    Console.WriteLine("\nKetik C untuk terhubung ke Database: ");
+                    char chr = Convert.ToChar(Console.ReadLine());
+                }
+            }
         }
     }
 }
